5c4a234baa5ce9de921364c2dc398907a911cbe9
VladimirFalkov <vladimirfalkov585@gmail.com>
2023-03-03 14:51:37 +0300

    fix bug

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Booking, Menu
+from .models import Booking, Menu
 
 # Register your models here.
 my_models = [Menu,Booking] # iterable list for multiple models
