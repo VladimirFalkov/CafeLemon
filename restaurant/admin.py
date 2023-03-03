58b20c53481df9b05f7d8fd984aa8b9a0a013769
VladimirFalkov <vladimirfalkov585@gmail.com>
2023-03-03 14:49:07 +0300

    regisrered models in admin

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from models import Booking, Menu
 
 # Register your models here.
+my_models = [Menu,Booking] # iterable list for multiple models
+admin.site.register(my_models)
