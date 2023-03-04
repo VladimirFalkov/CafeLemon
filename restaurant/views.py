8f9bd6aafc65d05f8bd9456c550a003266d90f23
VladimirFalkov <vladimirfalkov585@gmail.com>
2023-03-04 08:43:13 +0300

    created a booking view class

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,21 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Booking
+from.serializers import bookingSerializer
+
+
 
 # Create your views here.
-from django.http import HttpResponse
-
 def index(request):
  return render(request, 'index.html', {})
+
+
+class bookingview(APIView):
+
+    def get(self, request):
+       items = Booking.objects.all()
+       serializer = bookingSerializer(items, many=True)
+       return Response(serializer.data) # REturn JSON
