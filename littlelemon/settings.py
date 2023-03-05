a026b7498fa64b92cf98b6da84b101e3b56cdeaf
VladimirFalkov <vladimirfalkov585@gmail.com>
2023-03-05 13:58:05 +0300

    fixed bugs

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -129,15 +129,16 @@ STATICFILES_DIR = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES':
-        ['rest_framework.renders.JSONRenderer',
-        'rest_framework.renders.BrowsableAPIRenderer',
-        'rest_framework.renders.XMLRenderer',],
-    'DEFAULT_AUTHENTICATION_CLASSES':(
+REST_FRAMEWORK = { 
+    'DEFAULT_RENDERER_CLASSES':[
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        # 'rest_framework_xml.renderers.XMLRenderer',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES':[
         'rest_framework.authentication.TokenAuthentication',
-    ),
-    
+        'rest_framework.authentication.SessionAuthentication',
+    ]
 
 }
 
