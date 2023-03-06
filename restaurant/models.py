622c4b7f11ad547e73592795098cdb43579ef76e
VladimirFalkov <vladimirfalkov585@gmail.com>
2023-03-06 11:36:51 +0300

    made new presentation for data in Booking in admin-panel

--- restaurant/models.py
+++ restaurant/models.py
@@ -5,6 +5,9 @@ class Booking(models.Model):
     name= models.CharField(max_length=255)
     numb_of_guests = models.SmallIntegerField(6)
     booking_date = models.DateTimeField()
+    
+    def __str__(self) -> str:
+        return f'{self.name} booked for {str(self.numb_of_guests)} persons on {self.booking_date}'
 
 class Menu(models.Model):
     title = models.CharField(max_length=255)
